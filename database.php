c9bd1e4a52cd236a5b59d977822fa86dbe08549d
Abdul <abdulaziz_safi@yahoo.com>
2025-05-22 10:16:02 -0400

    coded Database Files

--- database.php
+++ database.php
@@ -1,5 +1,18 @@
 <?php
+    session_start();
+    $dsn = 'mysql:host=localhost;dbname=student_directory';
+    $username = 'root';
+    $password = '';
 
+    try {
+        $db = new PDO($dsn, $username, $password);
+    }
+    catch (PDOException $e)
+    {
+        $_SESSION["database_error"] = $e->getMessage();
+        $url = "database_error.php";
+        heder("Location: " . $url);
+        exit();
 
-
+    }
 ?>
--- database_error.php
+++ database_error.php
@@ -1,3 +1,28 @@
 <?php
-
+  session_start();
 ?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+      <title> Student Directory - Database Error</title>
+      <link rel="stylesheet" type="text/css" href="css/main.css" />
+ 
+  </head>
+  <body>
+      <?php include ("header.php"); ?>
+
+      <main>
+        <h2>Database Error</h2>
+
+        <p>There was an error connecting to the database.</p>
+        <p>The database must be installed.</p>
+        <p>MySQL must be running.</p>
+        <p>Error message: <?php echo $_SESSION["database_error"]; ?></p> 
+
+        <p><a href="index.php">View Student List</a></p>
+      </main> 
+
+      <?php include ("footer.php"); ?>
+  </body>
+</html>
